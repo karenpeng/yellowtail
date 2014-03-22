07cdab36fe72a6b12beb12f57b42b89814fd36a4
Karen <karenpenglabs@gmail.com>
2014-03-22 00:12:31 -0400

    x

--- sio_routes.js
+++ sio_routes.js
@@ -12,22 +12,23 @@ var firstId;
 module.exports = function (sio) {
   var pageOpen = 0;
   sio.sockets.on('connection', function (socket) {
-    if (pageOpen === 0) {
+    pageOpen++;
+    if (pageOpen === 1) {
       firstId = socket.id;
     }
     socket.on('myWorm', function (data) {
       sio.sockets.socket(firstId).emit('hisWorm', data);
       console.log("received!");
     });
-
-    pageOpen++;
     socket.emit('init', pageOpen);
 
     socket.broadcast.emit('in', pageOpen);
 
     socket.on('disconnect', function () {
-      firstId = null;
       pageOpen--;
+      if (pageOpen === 0) {
+        firstId = null;
+      }
       socket.broadcast.emit('leave', pageOpen);
     });
   });
