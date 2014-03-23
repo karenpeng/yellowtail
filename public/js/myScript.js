fd379e8ec1a6ceb68acbe8022b30d5f01fd17943
Karen <karenpenglabs@gmail.com>
2014-03-22 22:59:39 -0400

    x

--- public/js/myScript.js
+++ public/js/myScript.js
@@ -49,8 +49,19 @@ Worm.prototype = {
       //this.path.fullySelected = true;
     }
   },
+  check: function () {
+    var outCount = 0;
+    this.path.segments.forEach(function (s) {
+      if (s.point.x < 0 || s.point.x > view.size.width || s.point.y < 0 || s.point
+        .y > view.size.height) {
+        outCount++;
+      }
+    });
+    return outCount === this.path.segments.length;
+  },
   move: function () {
-    if (!mobile && this.mouseRelease && this.path.segments.length % 2 === 0) {
+    if (!mobile && this.mouseRelease && this.path.segments.length % 2 === 0 &&
+      this.path.segments.length > 3) {
       this.clock++;
       if (this.clock > 30) {
 
@@ -76,14 +87,6 @@ Worm.prototype = {
 
         this.path.closed = true;
         this.path.smooth();
-
-        // this.path.segments.forEach(function (s) {
-        //   console.log(s.point);
-        //   if (s.point.x < 0) s.point.x += view.size.x;
-        //   if (s.point.x > view.size.x) s.point.x -= view.size.x;
-        //   if (s.point.y < 0) s.point.y += view.size.y;
-        //   if (s.point.y > view.size.y) s.point.y -= view.size.y;
-        // })
       }
     }
   }
@@ -121,18 +124,24 @@ function onFrame(event) {
     socket.emit('myWorm', myWorm);
     worms = [];
   }
-  worms.forEach(function (w) {
-    w.move();
-  });
-  //document.getElementById("shakeShake").innerHTML = worms.length;
+
+  for (var i = 0; i < worms.length; i++) {
+    if (worms[i].check()) {
+      worms.splice(i, 1);
+    } else {
+      worms[i].move();
+    }
+  }
+  //document.getElementById("check").innerHTML = worms.length;
 }
 
 socket.on('hisWorm', function (data) {
-  //console.log(data);
+
+  var ranX = Math.random() * view.size.width / 2;
+  var ranY = Math.random() * view.size.height / 2;
+
   data.forEach(function (obj) {
-    console.log(obj);
     var newWorm = new Worm();
-    // newWorm.path = new Path();
     newWorm.path = new Path(obj.p);
     newWorm.mouseRelease = obj.m;
     obj.g.forEach(function (item) {
@@ -140,163 +149,11 @@ socket.on('hisWorm', function (data) {
       newWorm.gap.push(gapPoint);
     });
     newWorm.dis = new Point(obj.d[1], obj.d[2]);
+
+    newWorm.path.segments.forEach(function (item) {
+      item.point.x += ranX;
+      item.point.y += ranY;
+    });
     worms.push(newWorm);
   });
 });
-
-$(window).keydown(function (event) {
-  if (event.which === 32) {
-    event.preventDefault();
-    worms.forEach(function (w) {
-      console.log(w.path.segments);
-      w.path.segments.forEach(function (s) {
-
-        //console.log(s.point);
-      });
-    });
-  }
-});
-
-// (function (exports) {
-//   tool.minDistance = 1;
-//   tool.maxDistance = 100;
-//   var worm;
-//   var worms = [];
-//   var done = false;
-
-//   function Worm() {
-//     this.path = new Path();
-//     this.path.fillColor = new Color({
-//       hue: Math.random() * 360,
-//       saturation: 1,
-//       brightness: 1
-//     });
-//     this.mouseRelease = false;
-//     this.gap = [];
-//     this.dis = 0;
-//   }
-
-//   Worm.prototype = {
-//     beginPoint: function (p) {
-//       if (!this.mouseRelease) {
-//         this.path.add(p);
-//       }
-//     },
-//     pairPoint: function (d, m) {
-//       if (!this.mouseRelease) {
-//         this.gap.push(d);
-//         var step = d / 2;
-//         step.angle += 90;
-
-//         var top = m + step;
-//         var bottom = m - step;
-
-//         this.path.add(top);
-//         this.path.insert(0, bottom);
-//         this.path.smooth();
-//         //this.path.fullySelected = true;
-//       }
-//     },
-//     endPoint: function (p) {
-//       this.path.add(p);
-//       this.path.closed = true;
-//       this.path.smooth();
-//       this.mouseRelease = true;
-//       this.dis = this.path.lastSegment.point - this.path.segments[this.path.segments
-//         .length / 2 - 1].point;
-//       //this.path.fullySelected = true;
-//     },
-//     move: function () {
-//       if (!check && this.mouseRelease) {
-//         //this.path.fullySelected = true;
-
-//         // var newTop = this.path.segments[0].point - this.dis;
-//         // var newBottom = this.path.segments[this.path.segments.length - 2].point -
-//         //   this.dis;
-
-//         // this.path.insert(this.path.segments.length / 2 - 1, newTop);
-//         // this.path.removeSegment(0);
-//         // this.path.insert(this.path.segments.length / 2, newBottom);
-//         // this.path.removeSegment(this.path.segments.length - 2);
-
-//         // var newMiddleF = (this.path.segments[this.path.segments.length / 2]
-//         //   .point + this.path.segments[this.path.segments.length / 2 - 2].point
-//         // ) / 2;
-//         // this.path.segments[this.path.segments.length / 2 - 1].point =
-//         //   newMiddleF - this.gap[0];
-
-//         // var newMiddleB = (this.path.segments[0].point + this.path.segments[
-//         //   this.path.segments.length - 2].point) / 2;
-//         // this.path.lastSegment.point = newMiddleB + this.gap[this.gap.length -
-//         //   1];
-
-//         var newTop = this.path.segments[this.path.segments.length / 2 - 2].point +
-//           this.dis;
-//         var newBottom = this.path.segments[this.path.segments.length / 2].point +
-//           this.dis;
-
-//         this.path.removeSegment(this.path.segments.length / 2 - 2);
-//         this.path.insert(0, newTop);
-//         this.path.removeSegment(this.path.segments.length / 2);
-//         this.path.insert(this.path.segments.length - 1, newBottom);
-
-//         var newMiddleB = (this.path.segments[0].point + this.path.segments[
-//           this.path.segments.length - 2].point) / 2;
-//         this.path.lastSegment.point = newMiddleB + this.gap[this.gap.length -
-//           1];
-
-//         var newMiddleF = (this.path.segments[this.path.segments.length / 2]
-//           .point + this.path.segments[this.path.segments.length / 2 - 2].point
-//         ) / 2;
-//         this.path.segments[this.path.segments.length / 2 - 1].point =
-//           newMiddleF - this.gap[0];
-
-//         this.path.closed = true;
-//         this.path.smooth();
-
-//         // this.path.segments.forEach(function (s) {
-//         //   console.log(s.point);
-//         //   if (s.point.x < 0) s.point.x += view.size.x;
-//         //   if (s.point.x > view.size.x) s.point.x -= view.size.x;
-//         //   if (s.point.y < 0) s.point.y += view.size.y;
-//         //   if (s.point.y > view.size.y) s.point.y -= view.size.y;
-//         // })
-//       }
-//     }
-//   };
-
-//   //-----------------------------------------main--------------------------------------------
-//   exports.onMouseDown = function (event) {
-//     if (!done) {
-//       worm = new Worm();
-//       worm.beginPoint(event.point);
-//     }
-//   };
-
-//   exports.onMouseDrag = function (event) {
-//     if (!done) {
-//       worm.pairPoint(event.delta, event.middlePoint);
-//     }
-//   };
-
-//   exports.onMouseUp = function (event) {
-//     if (!done) {
-//       worm.endPoint(event.point);
-//       worms.push(worm);
-//       if (check) {
-//         done = true;
-//       }
-//     }
-//   };
-
-//   exports.onFrame = function (event) {
-//     if (shake) {
-//       $("#shakeShake").html("Yeah!");
-//     }
-//     worms.forEach(function (w) {
-//       w.move();
-//     });
-//   };
-
-//   exports.Worm = Worm;
-// })(this);
