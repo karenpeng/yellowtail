d4d0cb73bd6452e80fcbb2d9da5db9925e691857
Karen <karenpenglabs@gmail.com>
2014-03-24 01:36:32 -0400

    polishment

--- public/js/dom.js
+++ public/js/dom.js
@@ -2,43 +2,54 @@
   $("#myCanvas").hide();
   exports.begin = false;
 
-  setTimeout(
-    function () {
-      //nothing
-    }, 3000
-  );
-  setTimeout(
-    function () {
-      $("#welcome").hide();
-    }, 3001
-  );
+  if (mobile) {
+    setTimeout(
+      function () {
+        //nothing
+      }, 2600
+    );
+    setTimeout(
+      function () {
+        $("#welcome").hide();
+      }, 2601
+    );
 
-  setTimeout(
-    function () {
-      var couter = 0;
-      if (mobile) {
-        $("#mobileIntro").fadeIn();
-        $("#shake").hide();
-        $("#ok").click(function () {
-          couter++;
-          if (couter == 1) {
-            $("#draw").hide();
-            $("#shake").fadeIn();
-            $("#ok").html("Start");
-          } else if (couter == 2) {
-            $("#mobileIntro").hide();
-            $("#myCanvas").fadeIn();
-            exports.begin = true;
-          }
-        });
-      } else {
+    setTimeout(
+      function () {
+        var couter = 0;
+        if (mobile) {
+          $("#mobileIntro").fadeIn();
+          $("#shake").hide();
+          $("#ok").click(function () {
+            couter++;
+            if (couter == 1) {
+              $("#draw").hide();
+              $("#shake").fadeIn();
+              $("#ok").html("Start");
+            } else if (couter == 2) {
+              $("#mobileIntro").hide();
+              $("#myCanvas").fadeIn();
+              exports.begin = true;
+            }
+          });
+        }
+      }, 2602
+    );
+  } else {
+    setTimeout(
+      function () {
+        //nothing
+      }, 1000
+    );
+    setTimeout(
+      function () {
         $("#laptopIntro").fadeIn();
         $("#showQR").hide();
         $("#myCanvas").fadeIn();
         exports.begin = true;
-      }
-    }, 3020
-  );
+      }, 1001
+    );
+  }
 
   $("#showQR").click(function () {
     $("#QR").slideDown('fast');
